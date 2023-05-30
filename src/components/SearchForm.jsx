68fe0671e376b73483d308bd85b36876fff7f764
huski-commando <gyan.mu@gmail.com>
2023-05-30 19:49:55 +0530

    search-form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="button" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
